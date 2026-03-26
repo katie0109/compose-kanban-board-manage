30b426dbdee342210b00701755da532beca6f98a
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-03-26 13:38:40 +0900

    test: 태스크 완료 상태 변경 검증 테스트 추가 및 README 업데이트

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/KanbanBoardDataTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/KanbanBoardDataTest.kt
@@ -53,18 +53,13 @@ class KanbanBoardDataTest {
 
     @Test
     fun `태스크 전체 개수를 알고 있다`() {
-        val kanbanBoardData = KanbanBoardData(
-            boardList,
-        )
 
         Assertions.assertThat(kanbanBoardData.totalStatusCount()).isEqualTo(boardList.size)
     }
 
     @Test
     fun `상태(To-Do, In Progress, Done)별 태스크 개수가 노출된다`() {
-        val kanbanBoardData = KanbanBoardData(
-            boardList,
-        )
+
         Assertions.assertThat(kanbanBoardData.getStatusBoard(Status.TODO).size).isEqualTo(3)
         Assertions.assertThat(kanbanBoardData.getStatusBoard(Status.IN_PROGRESS).size).isEqualTo(1)
         Assertions.assertThat(kanbanBoardData.getStatusBoard(Status.DONE).size).isEqualTo(1)
@@ -72,9 +67,6 @@ class KanbanBoardDataTest {
 
     @Test
     fun `5개 중에 done이 1개라면 20%의 완료율을 계산한다`() {
-        val kanbanBoardData = KanbanBoardData(
-            boardList,
-        )
 
         Assertions.assertThat(kanbanBoardData.progress()).isEqualTo(0.2f)
     }
@@ -84,5 +76,12 @@ class KanbanBoardDataTest {
         val changeKanbanBoardData = kanbanBoardData.moveBoardDataStatus(taskId = targetCard.id, targetStatus = Status.IN_PROGRESS)
 
         assertThat(changeKanbanBoardData.boardList[0].status).isEqualTo(Status.IN_PROGRESS)
+    }
+
+    @Test
+    fun `상태를 To-Do에서 Done으로 옮겼을 때 doneCount가 증가한다`() {
+        val changeKanbanBoardData = kanbanBoardData.moveBoardDataStatus(taskId = targetCard.id, targetStatus = Status.DONE)
+
+        assertThat(changeKanbanBoardData.doneCount()).isEqualTo(2)
     }
 }
