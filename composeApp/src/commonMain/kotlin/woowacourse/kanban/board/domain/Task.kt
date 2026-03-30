ead7f7b80f2fc5fe212c5fa93002db51862d49c4
katie0109 <0109katie@naver.com>
2026-03-30 23:42:21 +0900

    refactor: task에서 tag, status 분리

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/Task.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/Task.kt
@@ -21,20 +21,3 @@ data class Task(
         fun isTagsError(tags: List<Tag>): Boolean = tags.size > MAX_TAGS_SIZE
     }
 }
-
-data class Tag(val text: String) {
-    init {
-        require(!isTagError(text)) { "[ERROR] 태그에서 오류가 발생했습니다." }
-    }
-
-    companion object {
-        const val MAX_TAG_LENGTH = 5
-        fun isTagError(tag: String): Boolean = tag.length > MAX_TAG_LENGTH || tag.isBlank() || tag.count { it.toString().isBlank() } > 0
-    }
-}
-
-enum class Status(val state: String) {
-    TODO("To Do"),
-    IN_PROGRESS("In Progress"),
-    DONE("Done"),
-}
