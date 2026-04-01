176c0ae275c437c0fe3017348bf2ccb0c2da5b81
katie0109 <0109katie@naver.com>
2026-04-01 17:44:50 +0900

    feat: 상태별 태스크 특징 정의

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/Status.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/Status.kt
@@ -6,3 +6,30 @@ enum class Status(val state: String) {
     REVIEW("Review"),
     DONE("Done"),
 }
+
+fun Status.isDeletable(): Boolean {
+    return when (this) {
+        Status.TODO, Status.IN_PROGRESS -> true
+        Status.REVIEW, Status.DONE -> false
+    }
+}
+
+fun Status.isAssigneeRequired():Boolean{
+    return when(this){
+        Status.TODO -> false
+        Status.IN_PROGRESS, Status.REVIEW, Status.DONE -> true
+    }
+}
+
+fun Status.isStatusTransitionAllowed(status: Status): Boolean {
+    return when (this) {
+        Status.TODO -> status == Status.IN_PROGRESS
+        Status.IN_PROGRESS -> status == Status.TODO || status ==Status.REVIEW
+        Status.REVIEW -> status == Status.IN_PROGRESS || status == Status.DONE
+        Status.DONE -> status == Status.TODO
+    }
+}
+
+fun Status.isStatusTransitionBlocked(status: Status): Boolean {
+    return !this.isStatusTransitionAllowed(status)
+}
